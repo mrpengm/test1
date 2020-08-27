02cd1024691f4e586c1eb2efa51c95f641ed0ec6
max <mrpengmforxmail.com>
2020-08-27 15:07:32 +0800

    添加文件

---
+++ HelloGit/src/main/java/cn/tedu/HelloGit.java
@@ -0,0 +1,12 @@
+package cn.tedu;
+
+/**
+ * @author mrpeng
+ * @description
+ * @create 2020 - 08 - 27 14:59
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
